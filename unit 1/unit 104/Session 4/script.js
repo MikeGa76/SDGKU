21772ee54a56e8635820a424584784f5a5a4219d
MikeGa76 <56799982+MikeGa76@users.noreply.github.com>
2019-12-11 17:11:57 -0500

    game finished

--- unit 1/unit 104/Session 4/script.js
+++ unit 1/unit 104/Session 4/script.js
@@ -12,17 +12,15 @@ class Character {
     this.id = id;
   }
   // display: character info
-  // add energy and item
   display = function() {
     document.getElementById(this.id).innerHTML = `<p> Name: ${this.name}</p> <p> Energy: ${this.energy}</p>`;
   }
   // attack opponent
-  // fix attack not method of character
-  attack = function(opponent) {
-    let newEnergy = opponent.energy - 20;
+  attack = function(opponent, item) {
+    let newEnergy = opponent.energy - item.iHitPoints;
     opponent.energy = newEnergy;
     opponent.display();
-    document.getElementById("console").innerHTML = `<p> ${this.name} is attacking ${opponent.name} with a ${item.name} ... </p>`
+    document.getElementById("console").innerHTML = `<p> ${this.name} is attacking ${opponent.name} with a ${item.iName} ... ${item.img}</p>`
     // console.log(character.attack)
     if (opponent.energy <= 0) {
       document.getElementById(opponent.id).innerHTML = `<p> You die`
@@ -30,7 +28,6 @@ class Character {
     }
   }
 }
-// end of constructor
 
 // created characters
 const character1 = new Character("Yoshi", 100, "c1")
@@ -38,7 +35,7 @@ const character2 = new Character("Toad", 100, "c2")
 character1.display();
 character2.display();
 // constructed items
-class item {
+class Item {
   constructor(iName, iHitPoints, img) {
     this.iName = iName;
     this.iHitPoints = iHitPoints;
@@ -46,20 +43,24 @@ class item {
   }
 }
 // created items
-/*
-const item1 = new Item("Goomba", 20, img / goomba.jpg)
-const item2 = new Item("Banana", 5, img / Banana.png)
-const item3 = new Item("Green Shell", 10, img / Greenshell.png)
+
+const item1 = new Item("Goomba", 20, '<img class="imgItem" src="img/goomba.jpg">')
+const item2 = new Item("Banana", 5, "<img class='imgItem' src='img/Banana.png'>")
+const item3 = new Item("Green Shell", 10, "<img class='imgItem' src='img/Greenshell.png'>")
 // items in an array
 const items = [item1, item2, item3]
 // selected item
 function selectItem() {
-  let selection = Math.floor(math.random() * 3)
+  let selection = Math.floor(Math.random() * 3)
   return items[selection];
 }
 selectItem()
-*/
-// reset the game
-const reset = function() {
 
+// reset the game
+const reset = function(char1, char2) {
+  char1.energy = 100
+  char2.energy = 100
+  char1.display();
+  char2.display();
+  document.getElementById("console").innerHTML = `Game has been reset!`
 }
---
+++ unit 1/unit 105/Session 1/myscript.js
@@ -0,0 +1,60 @@
+//object literal
+const salon = {
+  name: "The Fashion pets",
+  phone: "5512314",
+  address: {
+    street: "Av. Uni",
+    number: "262-c"
+  },
+  workingHours: {
+    days: "Mon-Fri",
+    open: "9:00 am",
+    close: "5:00 pm"
+  },
+  pets: []
+}
+
+//object destructuring
+let { name, phone, address: { street, number }, workingHours: { days, open, close } } = salon
+// console.log(name, street)
+document.getElementById("col1").innerHTML = `<h2 class="display-4"> ${name} </h2> <p>Phone Number: <br> ${phone} </p>`
+
+document.getElementById("col2").innerHTML = `<h2> Address Info </h2> <p class="lead"> Street: ${street}${number} </p>`
+
+document.getElementById("col2").innerHTML = `<p class="lead"> It is open from ${days} ${open} to ${close}.</p>`
+
+// object constructor
+class Pet {
+  constructor(name, age, breed, gender, service, ownerName, phoneContact) {
+    this.name = name;
+    this.age = age;
+    this.breed = breed;
+    this.gender = gender;
+    this.service = service;
+    this.ownerName = ownerName;
+    this.phoneContact = phoneContact;
+    this.hunger = 10;
+    this.happiness = 5;
+  }
+  ownerInfo = function() {
+    console.log("Owner Name: " + this.ownerName + "\n" + "Contact Phone: " + this.phone)
+  }
+  feed = function() {
+    this.hunger -= 10
+    this.happiness += 10
+    console.log("Feeding... ")
+  }
+  status = function() {
+    console.log("Hunger: " + this.hunger + "\n" + "Happiness: " + this.happiness)
+  }
+}
+const pet1 = new Pet("Shaggy", 2, "boxer", "male", "shower", "Sam", "90123");
+const pet2 = new Pet('asd', 2, 'ds', 'qwe', 'weq', '213', '125');
+console.log(salon)
+// pet1.feed()
+// pet1.status()
+// pet1.feed()
+// pet1.status()
+salon.pets.push(pet1)
+salon.pets.push(pet2)
+console.table(salon.pets)
